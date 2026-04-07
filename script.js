d1b484756e8537244ef4629ab9ed46fa930eb90b
PowerSpy2 <120425155+PowerSpy2@users.noreply.github.com>
2026-04-07 22:19:26 +0000

    1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // add javascript here
+let guess = 0;
+let answer = 0;
+let totalWins = 0;
+const scores = [];
+
+document.getElementById("playBtn").addEventListener
+("Click",play);
+
+function play(){
+    let range = 0;
+    let levels = document.getElementsByName("level");
+    for(let i=0; i<;levels.length; i++){
+            if)(levels[i].checked){
+                range = parseInt(levels[i].value);
+            }
+    }
+
+    document.getElementById("msg").textContent = "Guess a number 1-" + range; 
